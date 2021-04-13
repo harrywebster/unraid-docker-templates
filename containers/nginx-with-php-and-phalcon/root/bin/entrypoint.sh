2649babce77eb3dcfde6dd3b6ca59bdea0059c98
Harry Webster <harry@diffordsguide.com>
2021-04-13 23:50:50 +0100

    Replaced nginx container docker file

---
+++ containers/nginx-with-php-and-phalcon/root/bin/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ~/.bashrc
+
+# Start PHP daemon
+/etc/init.d/php7.2-fpm start
+
+# you must restart PHP FPm to get access to the ENV variables, i have no idea why?!
+/etc/init.d/php7.2-fpm restart
+
+# start up bash to use for access
+nginx -g "daemon off;"
