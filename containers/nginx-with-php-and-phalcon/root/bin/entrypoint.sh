ffe8980af970c011c5688ec1c0fb70c244907146
Harry Webster <harry@diffordsguide.com>
2021-04-17 23:26:38 +0100

    Added MySQL to NGINX continaer

--- containers/nginx-with-php-and-phalcon/root/bin/entrypoint.sh
+++ containers/nginx-with-php-and-phalcon/root/bin/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo Loading BASH profile
 source ~/.bashrc
 
-# Start PHP daemon
+echo Setting up MySQL database
+/bin/bash /root/bin/mysql.sh
+
+echo Starting PHP
 /etc/init.d/php7.4-fpm start
 
-# start up bash to use for access
+echo Starting NGINX webserver
 nginx -g "daemon off;"
