b66180e713c65be952fa1df9fb33f99b801c6b48
Harry Webster <harry@diffordsguide.com>
2021-04-27 20:47:08 +0100

    Added AWS s3 upload env varables.

--- containers/nginx-with-php-and-phalcon/root/bin/entrypoint.sh
+++ containers/nginx-with-php-and-phalcon/root/bin/entrypoint.sh
@@ -3,6 +3,17 @@
 echo Loading BASH profile
 source ~/.bashrc
 
+# Create AWS auth config
+mkdir /root/.aws
+echo "[default]" > /root/.aws/credentials
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> /root/.aws/credentials
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> /root/.aws/credentials
+echo "[default]" > /root/.aws/config
+echo "region=eu-west-2" >> /root/.aws/config
+echo "output=json" >> /root/.aws/config
+echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> /etc/environment
+echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> /etc/environment
+
 echo Setting up MySQL database
 /bin/bash /root/bin/mysql.sh
 
