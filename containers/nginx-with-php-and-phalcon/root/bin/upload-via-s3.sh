0a8bb5c18905d785bd53da13fd17642767137ac9
Harry Webster <harry@diffordsguide.com>
2021-04-27 22:12:46 +0100

    Fixed typo in S3 upload

--- containers/nginx-with-php-and-phalcon/root/bin/upload-via-s3.sh
+++ containers/nginx-with-php-and-phalcon/root/bin/upload-via-s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 sync /var/www/html/export/* s3://teach-keyboard-and-mouse/www/ --storage-class STANDARD
+aws s3 sync /var/www/html/export/ s3://teach-keyboard-and-mouse/www/ --storage-class STANDARD
 aws s3 sync /var/www/html/assets s3://teach-keyboard-and-mouse/www/assets --storage-class STANDARD
